d5d0a5ae65578f1c0a6dd227fc85218c87362584
Miki <you@example.com>
2019-12-15 13:27:46 +0100

    tworzenie nowego pliku do testow jednostkowych vol 2

--- testy_jednostkowe.py
+++ testy_jednostkowe.py
@@ -1,6 +1,23 @@
 #KOLEJNE ZADANIE
-def add(x, y):
-    return x+y
+# def add(x, y):
+#     return x+y
+#
+# def test_add():
+#     assert add(3, 5) == 8
+#     assert add(7, -1) == 6
+#     assert round(add(4.3, 5.6), 2) == 9.9
+#
+#
+# test_add()
+#
+#KOLEJNE ZADANIE Z TESTEM JEDNOSTKOWYM
+def product(x, y):
+    return x * y
 
-assert add(3, 5) == 8, "FAILED"
-assert add(7, -1) == 6,
+def test_product():
+    assert product(3, 5) == 15
+    assert product(1, 0) == 0
+    assert product(-8, 2.5) == -20
+
+
+test_product()
