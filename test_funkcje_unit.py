81ebd2a384923324b94539cb0545b218b94c0fe2
Miki <you@example.com>
2019-12-15 16:00:38 +0100

    koniec (nie robilem kalkulatora)

--- test_funkcje_unit.py
+++ test_funkcje_unit.py
@@ -21,3 +21,7 @@ class TestCircleArea(unittest.TestCase):
         self.assertRaises(TypeError, funkcje.circle_area, 3+5j)
         self.assertRaises(TypeError, funkcje.circle_area, True)
         self.assertRaises(TypeError, funkcje.circle_area, "asd")
+
+
+if __name__ == "__main__":
+    unittest.main()
